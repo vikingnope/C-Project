ba095bda4ad8c24c05d3344faf947c957e4acf68
Aiden Schembri <aiden.schembri.23@um.edu.mt>
2023-12-11 16:25:38 +0100

    updated to add x0

--- Question1.c
+++ Question1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void menu(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations);
-void secantMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations);
-void newtonRaphsonMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations);
+void menu(int *a, int *b, int *c, int *d, int *e, int *f, int *x0, int *iterations);
+void secantMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *x0, int *iterations);
+void newtonRaphsonMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *x0, int *iterations);
 
 int main() {
     int a, b, c, d, e, f;
-    int iterations;
+    int x0, iterations;
 
-    printf("Enter the polynomial in the format: x^5+bx^4+cx^3+dx^2+ex+f=0\n");
+    printf("Enter the polynomial in the format: Ax^5+Bx^4+Cx^3+Dx^2+Ex+F=0\n");
 
     printf("\n");
 
@@ -43,17 +43,26 @@ int main() {
 
     printf("\n");
 
+    printf("The polynomial is: %dx^5+%dx^4+%dx^3+%dx^2+%dx+%d=0\n", a, b, c, d, e, f);
+
+    printf("\n");
+
+    printf("Enter the starting value of x: ");
+    scanf("%d", &x0);
+
+    printf("\n");
+
     printf("Enter number of iterations: ");
     scanf("%d", &iterations);
 
     printf("\n");
 
-    menu(&a, &b, &c, &d, &e, &f, &iterations);
+    menu(&a, &b, &c, &d, &e, &f, &x0, &iterations);
 
     return 0;
 }
 
-void menu(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations){
+void menu(int *a, int *b, int *c, int *d, int *e, int *f, int *x0, int *iterations){
     int choice;
 
     printf("1. Secant Method\n");
@@ -72,31 +81,31 @@ void menu(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations){
 
     switch (choice){
         case 1:
-            secantMethod(a, b, c, d, e, f, iterations);
+            secantMethod(a, b, c, d, e, f, x0, iterations);
             break;
         case 2:
-            newtonRaphsonMethod(a, b, c, d, e, f, iterations);
+            newtonRaphsonMethod(a, b, c, d, e, f, x0, iterations);
             break;
         case 3:
             printf("Thank you for using this program.\n");
             break;
         default:
             printf("Invalid choice from menu.\n");
-            menu(a, b, c, d, e, f, iterations);
+            menu(a, b, c, d, e, f, x0, iterations);
     }
 }
 
-void secantMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations){
+void secantMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *x0, int *iterations){
 
     printf("Secant Method\n");
 
 
-    menu(a, b, c, d, e, f, iterations);
+    menu(a, b, c, d, e, f, x0, iterations);
 }
 
-void newtonRaphsonMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *iterations){
+void newtonRaphsonMethod(int *a, int *b, int *c, int *d, int *e, int *f, int *x0, int *iterations){
 
     printf("Newton-Raphson Method\n");
 
-    menu(a, b, c, d, e, f, iterations);
+    menu(a, b, c, d, e, f, x0, iterations);
 }
